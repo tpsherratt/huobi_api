2438ddb44ee8153435008b768e563bfbcb8b48d6
Timothy Sherratt <tim@mitoo.co>
2018-03-02 16:54:49 +0000

    add balance endpoint

--- lib/huobi_api/accounts.rb
+++ lib/huobi_api/accounts.rb
@@ -4,5 +4,10 @@ module HuobiApi
       endpoint = "/v1/account/accounts"
       get(endpoint, {})
     end
+
+    def balances
+      endpoint = "/v1/account/accounts/#{HuobiApi.account_id}/balance"
+      get(endpoint, {})
+    end
   end
 end
